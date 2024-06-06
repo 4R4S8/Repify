bc14cd0341b849961557633b44d046b7c2a0bc7a
4R4S8 <4R4S8@EYZEE>
2024-06-06 13:23:07 +0330

    (DONE) Change the applications assembly info

--- FitnessTrackerApp/Properties/AssemblyInfo.cs
+++ FitnessTrackerApp/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FitnessTrackerApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("4R4S8")]
 [assembly: AssemblyProduct("FitnessTrackerApp")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
