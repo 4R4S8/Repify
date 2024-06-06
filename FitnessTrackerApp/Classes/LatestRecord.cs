0739887b2bd89d117bbfd177fe30923ad0d644e0
4R4S8 <4R4S8@EYZEE>
2024-06-06 13:21:41 +0330

    (DONE) add new class to wrap the latest records data as an object

---
+++ FitnessTrackerApp/Classes/LatestRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace FitnessTrackerApp.Classes
+{
+    internal class LatestRecord
+    {
+        public string RoutinName { get; set; }
+        public int TotalSet { get; set; }
+        public string TotalWeight { get; set; }
+        public string Time { get; set; }
+    }
+}
